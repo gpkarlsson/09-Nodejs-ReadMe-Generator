e2b3a7c2a122c23e1402da68c103a565d7a3e5c8
Gordon Karlsson <gpkarlsson72@gmail.com>
2023-01-24 16:05:35 -0600

    added inquirer package, woring on readme generator questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,15 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+console.log('readme gen running');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        default: 'Node.js Professional Readme Generator'
+        //validate input?
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
